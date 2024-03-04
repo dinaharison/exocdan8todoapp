04b04edcd5d592200a17fad3a067a9c123502e51
dinaharison <dinaharison.jb@gmail.com>
2024-03-04 16:13:40 +0300

    adding branches

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using exocdan8todoapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace exocdan8todoapp.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Utilisateur utilisateur)
+        {
+            return RedirectToAction("Dashboard");
+        }
+    }
+}
---
+++ Models/Utilisateur.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace exocdan8todoapp.Models
+{
+    public class Utilisateur
+    {
+
+        private int _id;
+        private string _username;
+        private string _motDePasse;
+
+        public Utilisateur()
+        {
+        }
+
+        public Utilisateur(int id, string username, string motDePasse)
+        {
+            _id = id;
+            _username = username;
+            _motDePasse = motDePasse;
+        }
+
+        public int Id { get => _id; set => _id = value; }
+        public string Username { get => _username; set => _username = value; }
+        public string MotDePasse { get => _motDePasse; set => _motDePasse = value; }
+    }
+}
